7d133e331ed6e6b81cdf3a9cf4971b41cca73a34
jeshin <jsshin0201@gmail.com>
2024-08-24 12:26:41 +0900

    chore: bkup

---
+++ ex03/ScavTrap.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef __SCAVTRAP_H__
+#define __SCAVTRAP_H__
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+    public:
+        ScavTrap();
+        ScavTrap(const std::string name);
+        ScavTrap(const ScavTrap& rhs);
+
+        virtual ~ScavTrap();
+
+        virtual void    attack(const std::string& target);
+        virtual void    takeDamage(unsigned int amount);
+        virtual void    beRepaired(unsigned int amount);
+        void    guardGate();
+
+        ScavTrap&   operator=(const ScavTrap& rhs);
+    private:
+        static const unsigned int _kHitPoint;
+        static const unsigned int _kEnergyPoint;
+        static const unsigned int _kAttackDamage;
+
+        virtual bool    _checkCanDoSomething() const;
+};
+#endif
